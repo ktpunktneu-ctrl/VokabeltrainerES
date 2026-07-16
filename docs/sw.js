61ce6b55eccd86e8e4bac30e40b4ae3e500c3349
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-16 22:47:29 +0200

    Install-Banner: In-App-Browser-Erkennung (Insta/FB/TikTok) + PWA-Install-Hinweis

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabeles-v1';
+const CACHE = 'vokabeles-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './static/icon-192.png', './static/icon-512.png'];
 
 self.addEventListener('install', e => {
