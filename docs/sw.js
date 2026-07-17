2edbbb63fa723f2a36d8d726ab33641cbb728971
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-17 08:05:47 +0200

    Fix: eigenes ES-Flaggen-Icon statt kopiertem EN-Icon, Cache-Version erhöht

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabeles-v2';
+const CACHE = 'vokabeles-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './static/icon-192.png', './static/icon-512.png'];
 
 self.addEventListener('install', e => {
