fe0ced019c9ecdf29e56b2c6c63f4ef4d4e19b68
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-08-18 20:36:57 +0200

    Spaced-Repetition (Leitner-Boxen): faellige Vokabeln werden im Quiz bevorzugt abgefragt
    Pro Vokabel/Verbform neue Felder box+faelligAm, richtig -> hoehere Box mit laengerem
    Intervall, falsch -> zurueck auf Box 1. Docs-Ordner (GitHub-Pages-Kopie) synchronisiert.

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabeles-v7';
+const CACHE = 'vokabeles-v8';
 const ASSETS = ['./', './index.html', './manifest.json', './static/icon-192.png', './static/icon-512.png', './static/fonts/designer.woff2'];
 
 self.addEventListener('install', e => {
